f63bae9cc8dd4c017be7582a60eace6853b0edf6
rodgersxy <rodgersny99@gmail.com>
2023-06-23 14:11:18 -0700

    fix some bugs

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,7 +10,10 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int temp = 0;
+
+	if (array == NULL || size == 0)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -18,9 +21,9 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
 				print_array(array, size);
 			}
 		}
